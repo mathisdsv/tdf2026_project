cfae1c4ecbd0850dd89d778211d96b807877e4a7
Mathis Da silva <mathis.dasilva23@gmail.com>
2025-12-09 09:40:33 +0000

    1 ere vague de cleaning

--- models/staging/tdf_source/stg_tdf_source__rider_information.sql
+++ models/staging/tdf_source/stg_tdf_source__rider_information.sql
@@ -13,11 +13,11 @@ renamed as (
         year,
         weight,
         height,
-        onedayraces,
+        onedayraces as one_day_races,
         onedayraces_bar,
-        gc,
+        gc as general_classement,
         gc_bar,
-        tt,
+        tt as time_trial,
         tt_bar,
         sprint,
         sprint_bar,
@@ -25,12 +25,38 @@ renamed as (
         climber_bar,
         hills,
         hills_bar,
-        wins,
-        grand_tours,
-        classics
+        wins as nb_wins,
+        grand_tours as grand_tours_participation,
+        classics as classics_participation
 
     from source
 
+),
+
+cleaning as (
+    select 
+INITCAP(REPLACE(REPLACE(riderhref, 'rider/', ''), '-', ' ')) as rider_name,
+year,
+REPLACE(weight, 'kg','') as weight_kg,
+REPLACE(height, 'm','') as height_m,
+one_day_races,
+REPLACE(onedayraces_bar,"w","") as onedayraces_bar,
+general_classement,
+REPLACE(gc_bar,"w","") as gc_bar,
+time_trial,
+REPLACE(tt_bar,"w","") as tt_bar,
+sprint,
+REPLACE(sprint_bar,"w","") as sprint_bar,
+climber,
+REPLACE(climber_bar,"w","") as climber_bar,
+hills,
+REPLACE(hills_bar,"w","") as hills_bar,
+nb_wins, 
+grand_tours_participation,
+classics_participation
+        -- Tes transformations ici
+
+    from renamed
 )
 
-select * from renamed
+select * from cleaning
