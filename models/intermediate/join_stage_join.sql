8b5f62f8e13bebe228097fcffda63c16a0d00a44
Gabriel Storez <gabriel.storez@gmail.com>
2025-12-10 09:09:48 +0000

    stage_results : Safe Cast sur toutes les colonnes join_stage_join.sql : join incomplet

--- models/intermediate/join_stage_join.sql
+++ models/intermediate/join_stage_join.sql
@@ -1,3 +1,4 @@
-Select * 
-
-From 
+Select 
+    * 
+From {{ ref('stg_tdf_source__stage_information') }} AS stage_info
+    Full Outer join {{ ref('stg_tdf_source__stage_results') }} AS stage_results  
