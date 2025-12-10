132bc1b13bb53b75ff47a5cd44790110f60104ea
Mayeur Mathias <mathmay07@gmail.com>
2025-12-10 11:02:57 +0000

    (rider_info+rider_result) join / agregg : rider_per_yer

---
+++ models/intermediate/Rider_per_year.sql
@@ -0,0 +1,21 @@
+WITH join_join AS (
+select * except (year)
+from {{ ref('stg_tdf_source__rider_results') }}
+inner join {{ ref('stg_tdf_source__rider_information') }}
+using (rider_name)
+)
+select
+rider_name,
+year_year,
+ROUND(SUM(distance),2) as tot_distance,
+COUNT(Distinct distance) as nb_races,
+SUM(pointspcs) as tot_pointsPCS,
+SUM(pointsuci) as tot_pointsUCI,
+AVG(CAST(weight_kg as FLOAT64)) as weight_kg,
+AVG(CAST(height_m as FLOAT64)) as height_m,
+AVG(time_trial) as time_trial,
+AVG(sprint) as sprint,
+AVG(climber) as climber,
+AVG(hills) as hills,
+From join_join
+GROUP BY rider_name,year_year
