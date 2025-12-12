7262eda521543adff7433b2108b3ce997f172907
Mathis Da silva <mathis.dasilva23@gmail.com>
2025-12-12 10:58:32 +0000

    ggggg

--- models/mart/final_table.sql
+++ models/mart/final_table.sql
@@ -6,7 +6,40 @@ using (rider_name)
 ),
 
 finish as(
-select *
+select * except(year, stage_title, profile_score, time, classics_participation ),
+year as year_year,
 from join_join
+order by year_year, stage, rank ASC
+
+),
+
+finish_2 as(
+select 
+rider_name,
+team,
+year_year,
+stage,
+rank,
+points,
+general_classement,
+specialty,
+age,
+parcours_type,
+distance_km,
+avg_speed_kmh,
+final_km_gradient,
+vertical_meters,
+startlist_quality_score,
+win_type,
+nb_races,
+nb_wins,
+weight_kg,
+height_m,
+grand_tours_participation, 
+efficiency,
+win_rate,
+
+from finish 
 )
-select * from finish 
+
+select * from finish_2 
