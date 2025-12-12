274c76d55616799e6684cc62288d86fea917a285
yanis louison <yanis_94370@hotmail.fr>
2025-12-12 09:30:53 +0000

    join with parcours type

---
+++ models/intermediate_stages/join_with_type.sql
@@ -0,0 +1,106 @@
+{{ config(materialized="table") }}
+
+WITH results AS (
+
+    SELECT
+        year,
+        stage_number,
+        stage_title AS stage_title_raw,
+
+        rank,
+        general_classement,
+        timelag,
+        n_dossard,
+        specialty,
+        age,
+        rider_name,
+        team,
+        points,
+        time
+    FROM {{ ref('stg_tdf_source__stage_results') }}
+
+),
+
+stages AS (
+
+    SELECT
+        year,
+        stage_number,
+        stage_date,
+        avg_speed_kmh,
+        distance_km,
+        final_km_gradient,
+        profile_score,
+        vertical_meters,
+        departure_city,
+        arrival_city,
+        startlist_quality_score,
+        win_type
+    FROM {{ ref('stg_tdf_source__stage_information') }}
+
+),
+
+cleaned_results AS (
+
+    SELECT
+        year,
+        stage_number,
+
+        -- descriptif uniquement
+        TRIM(stage_title_raw) AS stage_title,
+
+        rank,
+        general_classement,
+        timelag,
+        n_dossard,
+        specialty,
+        age,
+
+        INITCAP(
+            REPLACE(REPLACE(rider_name, 'rider/', ''), '-', ' ')
+        ) AS rider_name,
+
+        team,
+        points,
+        time
+    FROM results
+
+),
+
+joined AS (
+
+    SELECT
+        -- clés
+        r.year,
+        r.stage_number,
+        r.stage_title,
+
+        -- Infos coureurs
+        r.rank,
+        r.general_classement,
+        r.specialty,
+        r.age,
+        r.rider_name,
+        r.team,
+        r.points,
+        r.time,
+
+        -- Infos étape
+        s.stage_date,
+        s.distance_km,
+        s.avg_speed_kmh,
+        s.final_km_gradient,
+        s.profile_score,
+        s.vertical_meters,
+        s.startlist_quality_score,
+        s.win_type
+
+    FROM cleaned_results r
+    LEFT JOIN stages s
+        ON r.year = s.year
+       AND r.stage_number = s.stage_number
+
+)
+
+SELECT *
+FROM joined
